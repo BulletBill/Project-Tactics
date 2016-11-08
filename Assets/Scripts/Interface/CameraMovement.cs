504c4bbebef0d515e546e3e0f9c8f50419fd68cb
Bill Caldwell <xiaolinslacker@gmail.com>
2016-11-08 11:41:12 -0600

    Added camera movement with keyboard input

--- Assets/Scripts/Interface/CameraMovement.cs
+++ Assets/Scripts/Interface/CameraMovement.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
+	public float KeyboardScrollSpeed = 1.0f;
+	public float MouseScrollSpeed = 1.0f;
+
+	Camera parentCamera;
+
 	// Use this for initialization
 	void Start () {
-	
+		parentCamera = this.gameObject.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 translateVector = new Vector3(0, 0, 0);
+		translateVector.x = Input.GetAxis("Horizontal") * KeyboardScrollSpeed * Time.deltaTime;
+		translateVector.y = Input.GetAxis("Vertical") * KeyboardScrollSpeed * Time.deltaTime;
+
+		parentCamera.transform.Translate(translateVector);
 	}
 }
