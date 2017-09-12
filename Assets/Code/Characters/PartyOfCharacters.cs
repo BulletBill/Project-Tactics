41a2e3c9e857ab64dc534ea50a3518a23479e263
Bill Caldwell <xiaolinslacker@gmail.com>
2017-09-12 09:07:11 -0500

    Added art assets

---
+++ Assets/Code/Characters/PartyOfCharacters.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartyOfCharacters : MonoBehaviour {
+
+	public List<PartyMember> PartyMembers = new List<PartyMember>();
+
+	// Use this for initialization
+	void Awake() {
+		//InitParty();
+	}
+
+	void Start () {
+		foreach (var p in PartyMembers) {
+			p.Start();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitParty() {
+		PartyMembers = new List<PartyMember>();
+
+		const int PartySize = 4;
+		for (int i = 0; i < PartySize; i++) {
+			PartyMembers.Add(new PartyMember());
+		}
+
+		//Temp init
+		PartyMembers[0].Name = "Mandolin";
+		PartyMembers[1].Name = "Brom";
+		PartyMembers[2].Name = "Silver Winter";
+		PartyMembers[3].Name = "Kabui";
+	}
+
+	void OnValidate() {
+		foreach (var p in PartyMembers) {
+			p.OnValidate();
+		}
+	}
+}
